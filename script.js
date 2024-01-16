1c67e55a864930bc261dbbf7d0e7f4dbaf60770d
Neo319 <alexmnevins@gmail.com>
2024-01-16 12:13:57 -0500

    implement a check for wins

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ const gameController = (function() {
         if (!move) {
             console.log("Space is occupied. Choose a different space.");
         } else {
+            checkForWins();
             cpuTurn();
         }
         return move;
@@ -75,11 +76,57 @@ const gameController = (function() {
 
             // successful move was made
             if (move) {
+                checkForWins();
                 return move;
             }
         }
-
     }
+
+    // check all possible wins and draws, ending game if needed.
+    function checkForWins () {
+
+        //check columns
+        for (let i = 0; i < 3; i++) {
+            if (
+                board[0][i] !== '' &&
+                board[0][i] === board[1][i] &&
+                board[1][i] === board[2][i] 
+                ) {
+                    console.log(`col ${i} win`)
+                    win(board[0][1]);
+                    return board[0][i]; //the winning player
+                }
+
+        // check rows
+            else if (
+                board[i][0] !== '' &&
+                board[i][0] === board[i][1] &&
+                board[i][1] === board[i][2] 
+                ) {
+                    console.log (`row ${i} win`)
+                    win(board[i][0]);
+                    return board[i][0]; //the winning player
+                }
+        }
+
+        //check diagonals
+        if (
+            board[1][1] !== '' && (
+            board[0][0] === board [1][1] &&
+            board[1][1] === board [2][2] ||
+            board[0][2] === board [1][1] &&
+            board[1][1] === board [2] )
+        ) {
+            console.log("diagonal win")
+            win(board[1][1]);
+            return board[1][1]; //the winning player
+        }
+    }
+
+    function win(player) {
+        console.log("game over...")
+    }
+
     return {playerMove};
 })()
 
